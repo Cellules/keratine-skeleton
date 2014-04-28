b038329be46670cfe05d929026426bef581f87be
Quentin Aupetit <quentin@cellules.tv>
2014-04-28 11:37:31 +0200

    removed useless index_dev.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\Debug\Debug;
 
-ini_set('display_errors', 0);
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+// if (isset($_SERVER['HTTP_CLIENT_IP'])
+//     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+// ) {
+//     header('HTTP/1.0 403 Forbidden');
+//     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+// }
 
 $loader = require_once __DIR__.'/vendor/autoload.php';
 
@@ -14,4 +23,10 @@ AnnotationRegistry::registerFile(__DIR__.'/vendor/doctrine/orm/lib/Doctrine/ORM/
 
 $app = require __DIR__.'/src/app.php';
 require __DIR__.'/src/controllers.php';
-$app->run();
+
+if ($app['debug']) {
+    $app->run();
+} else{
+    Debug::enable();
+    $app['http_cache']->run();
+}
